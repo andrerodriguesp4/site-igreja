891772ea2bb9eb264752f48ee766bd577456a8f0
andrerodriguesp4 <andrerodrigues2016p4@gmail.com>
2025-04-09 14:54:45 -0300

    Correção

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -4,18 +4,18 @@ import H1title from '../components/css/H1title.jsx'
 function Contato(){
     return(
         <div className='flex justify-center'>
-            <div className='pb-10 px-6'>
+            <div className='pb-10 px-2'>
                 <div className='flex justify-center items-center'>
                     <Send size={30}/>
                     <H1title>Contato</H1title>
                 </div>
                 <div className='bg-red-200 rounded-md border'>
-                    <div className='flex items-center gap-2 py-2 px-8 hover:bg-green-300 hover:cursor-default'>
+                    <div className='flex items-center gap-2 py-2 px-4 hover:bg-green-300 hover:cursor-default'>
                         <Phone/>
                         <h2 className='text-base'>Tel: <span className='text-sm'><em>(35) 3371-1788</em></span></h2>
 
                     </div>
-                    <div className='flex items-center gap-2 py-2 px-8 hover:bg-green-300 hover:cursor-default'>
+                    <div className='flex items-center gap-2 py-2 px-4 hover:bg-green-300 hover:cursor-default'>
                         <Mail/>
                         <h2 className='text-base'>E-mail: <span className='text-smxl'><em>departamentopessoal@familiacontabil.com</em></span></h2>
                     </div>
